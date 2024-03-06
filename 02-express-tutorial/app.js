1f798fa193677cac99692416fc9378865bdec3f2
Volodymyr Pisotskyi <volodymyr.pisotskyi@volodymyrs-mbp.lan>
2024-03-05 22:15:03 -0800

    finish mar 5

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,21 +1,10 @@
 const express = require("express");
-const path = require("path");
 const app = express();
-const port = 5432;
 
-// app.use for setting middleware
-app.use(express.static("./public"));
-
-// app.get("/", (req, res) => {
-// 	console.log(`homepage hit`);
-// 	res.status(200).sendFile(path.resolve(__dirname, "./navbar-app/index.html"));
-//     adding static asset
-// });
-
-app.all("*", (req, res) => {
-	res.status(400).send("<h1>Page not found</h1>");
+app.get("/", (req, res) => {
+	res.json();
 });
 
-app.listen(port, () => {
-	console.log(`listening to port ${port}...`);
+app.listen(5432, () => {
+	console.log(`listening on port 5432`);
 });
--- 02-express-tutorial/final/04-express-app.js
+++ 02-express-tutorial/final/04-express-app.js
@@ -1,19 +1,41 @@
-const express = require('express')
-const path = require('path')
+const express = require("express");
+const path = require("path");
 
-const app = express()
+const app = express();
 
 // setup static and middleware
-app.use(express.static('./public'))
+app.use(express.static("./public"));
 
-app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, './navbar-app/index.html'))
-})
+app.get("/", (req, res) => {
+	res.sendFile(path.resolve(__dirname, "./navbar-app/index.html"));
+});
 
-app.all('*', (req, res) => {
-  res.status(404).send('resource not found')
-})
+app.all("*", (req, res) => {
+	res.status(404).send("resource not found");
+});
 
 app.listen(5000, () => {
-  console.log('server is listening on port 5000....')
-})
+	console.log("server is listening on port 5000....");
+});
+
+// const express = require("express");
+// const path = require("path");
+// const app = express();
+// const port = 5432;
+
+// // app.use for setting middleware
+// app.use(express.static("./public"));
+
+// // app.get("/", (req, res) => {
+// // 	console.log(`homepage hit`);
+// // 	res.status(200).sendFile(path.resolve(__dirname, "./navbar-app/index.html"));
+// //     adding static asset
+// // });
+
+// app.all("*", (req, res) => {
+// 	res.status(400).send("<h1>Page not found</h1>");
+// });
+
+// app.listen(port, () => {
+// 	console.log(`listening to port ${port}...`);
+// });
