0beb6af4dd16167ac1a9490d6be24a375213d4e6
Volodymyr Pisotskyi <volodymyr.pisotskyi@volodymyrs-mbp.lan>
2024-03-05 20:01:48 -0800

    basic express server setup

--- 02-express-tutorial/final/03-express-basics.js
+++ 02-express-tutorial/final/03-express-basics.js
@@ -1,22 +1,22 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
-app.get('/', (req, res) => {
-  console.log('user hit the resource')
-  res.status(200).send('Home Page')
-})
+app.get("/", (req, res) => {
+	console.log("user hit the resource");
+	res.status(200).send("Home Page");
+});
 
-app.get('/about', (req, res) => {
-  res.status(200).send('About Page')
-})
+app.get("/about", (req, res) => {
+	res.status(200).send("About Page");
+});
 
-app.all('*', (req, res) => {
-  res.status(404).send('<h1>resource not found</h1>')
-})
+app.all("*", (req, res) => {
+	res.status(404).send("<h1>resource not found</h1>");
+});
 
 app.listen(5000, () => {
-  console.log('server is listening on port 5000...')
-})
+	console.log("server is listening on port 5000...");
+});
 
 // app.get
 // app.post
@@ -25,3 +25,34 @@ app.listen(5000, () => {
 // app.all
 // app.use
 // app.listen
+
+// const express = require("express");
+// require("colors");
+// const app = express();
+// const port = 5432;
+
+// app.get("/", (req, res) => {
+// 	console.log("home page hit".cyan);
+// 	res.status(200).send("Home Page");
+// });
+
+// app.get("/about", (req, res) => {
+// 	console.log("about page hit".cyan);
+// 	res.status(200).send("About Page");
+// });
+
+// app.all("*", (req, res) => {
+// 	res.status(400).send("<h1>page not found</h1>");
+// });
+
+// app.listen(port, () => {
+// 	console.log(`server is listening on port ${port}`.magenta);
+// });
+
+// // app.get
+// // app.post
+// // app.put
+// // app.delete
+
+// // app.all
+// // app.use
