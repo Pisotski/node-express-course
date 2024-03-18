1b88433eb8ae7a2f6f689d4776f21fb7dd803c6b
Volodymyr Pisotskyi <volodymyr.pisotskyi@volodymyrs-mbp.lan>
2024-03-18 12:27:44 -0700

    basic app+router setup

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,1 +1,16 @@
-console.log('Task Manager App')
+const express = require("express");
+const router = require("./routes/router");
+const { test } = require("./controllers/taskControllers");
+
+const app = express();
+const port = 5432;
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static("./public"));
+app.use("/api/v1", router);
+
+app.get("*", (_, res) => {
+	res.status(404).send(`<h2>page not found</h2>`);
+});
+
+app.listen(port, () => console.log(`listening on port ${port}...`));
