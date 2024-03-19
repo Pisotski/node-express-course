0cd6c1848a2d2c51474e7e806a30bfdc4230d34f
Volodymyr Pisotskyi <volodymyr.pisotskyi@volodymyrs-mbp.lan>
2024-03-18 17:05:07 -0700

    database connected

--- 03-task-manager/starter/controllers/taskControllers.js
+++
@@ -1,4 +0,0 @@
-const test = (req, res) => {
-	res.send(`<h2>test</h2>`);
-};
-module.exports = { test };
--- 03-task-manager/starter/routes/router.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { test } = require("../controllers/taskControllers");
-
-const router = express.Router();
-
-router.route("/").get(test).post();
-router.route("/:id").get(test).post().patch().delete();
-
-module.exports = router;
