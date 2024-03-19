2885b81a8e0ab1ddfb24cb21c19f46a114fd33d0
Volodymyr Pisotskyi <volodymyr.pisotskyi@volodymyrs-mbp.lan>
2024-03-18 17:05:07 -0700

    database connected

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,16 +1,27 @@
 const express = require("express");
-const router = require("./routes/router");
-const { test } = require("./controllers/taskControllers");
+const { connectDB } = require("./db/connect");
+const tasks = require("./routes/tasks");
+require("dotenv").config();
 
 const app = express();
 const port = 5432;
 
+const start = async () => {
+	try {
+		await connectDB(process.env.MONGO_URI);
+		app.listen(port, () => console.log(`listening on port ${port}...`));
+	} catch (error) {
+		console.log(error);
+	}
+};
+
 app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 app.use(express.static("./public"));
-app.use("/api/v1", router);
+app.use("/api/v1/tasks", tasks);
 
 app.get("*", (_, res) => {
 	res.status(404).send(`<h2>page not found</h2>`);
 });
 
-app.listen(port, () => console.log(`listening on port ${port}...`));
+start();
