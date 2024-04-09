ae1b7333f1ad2c6be594cc7e7a2f2731333cd399
Pisotski <vlad.pisotski@gmail.com>
2024-04-09 10:51:58 -0700

    client-server established connections

--- 05-JWT-Basics/preferred/public/index.js
+++ 05-JWT-Basics/preferred/public/index.js
@@ -1,1 +1,27 @@
-const loginEl = document.querySelector("#auth");
+const API = `http://localhost:4322/api/v1`;
+const form = document.querySelector("#auth-form");
+const input = document.querySelector("#auth-input");
+
+const serverCall = async (endpoint, method = "GET", headers = {}, body) => {
+	try {
+		const url = API + endpoint;
+		const options = { method, headers, body };
+		const response = await fetch(url, options);
+		const result = await response.json();
+		console.log(result);
+	} catch (error) {
+		console.error("Error:", error);
+	}
+};
+const sendPassword = (e) => {
+	e.preventDefault();
+	const inputValue = JSON.stringify({ pwd: input.value });
+	serverCall(
+		"/logon",
+		"POST",
+		{ "Content-Type": "application/json" },
+		inputValue
+	);
+};
+
+form.addEventListener("submit", sendPassword);
--- 05-JWT-Basics/preferred/routes/main.js
+++ 05-JWT-Basics/preferred/routes/main.js
@@ -6,8 +6,8 @@ const { middleware } = require("../middleware/middleware");
 router.get("/hello", middleware, getHelloController);
 
 router.post("/logon", (req, res) => {
-	console.log(`post request`);
-	res.send("posted");
+	console.log(req.body);
+	res.status(200).json("user logged in");
 });
 
 module.exports = router;
